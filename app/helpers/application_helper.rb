dad1da3e534aa04494220da00db5a2e078faf4ce
ACV <avandeg@gmail.com>
2011-02-13 10:31:23 -0700

    Done with sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,19 @@
 module ApplicationHelper
 
-    # Return a title on a per-page basis.
-    def title
-      base_title = "Ruby on Rails Tutorial Sample App"
-      if @title.nil?
-        base_title
-      else
-        "#{base_title} | #{@title}"
-      end
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
     end
+  end
+    
+  def logo
+    image_tag("logo.png", :alt => "Moloko!", :class => "round", :width => "90")
+  end
+    
+  #<% logo = image_tag("logo.png", :alt => "Moloko!", :class => "round", :width => "90") %>
 
 end
